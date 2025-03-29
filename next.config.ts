bb72a600d17c2c3465e8efec80e4c591b53e9243
Pedro Guilherme <pedroguinasc2002@gmail.com>
2025-03-29 20:28:27 -0300

    fix: deploy at vercel

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,10 @@ import { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 import path from "path";
 
+const withNextIntl = createNextIntlPlugin();
+
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  i18n: {
-    locales: ["en", "pt", "es"],
-    defaultLocale: "en",
-    localeDetection: false,
-  },
   webpack: (config) => {
     config.resolve.alias["@"] = path.resolve(__dirname);
     config.resolve.alias["@/public"] = path.resolve(__dirname, "public");
@@ -19,21 +16,9 @@ const nextConfig: NextConfig = {
     );
     config.resolve.alias["@/utils"] = path.resolve(__dirname, "src/utils");
     config.resolve.alias["@/styles"] = path.resolve(__dirname, "src/styles");
-
     return config;
   },
   images: {
-    localPatterns: [
-      {
-        pathname: "./public/**",
-      },
-      {
-        pathname: "./src/brand/**",
-      },
-      {
-        pathname: "/media/**",
-      },
-    ],
     remotePatterns: [
       {
         protocol: "https",
@@ -41,11 +26,9 @@ const nextConfig: NextConfig = {
           process.env.NEXT_PUBLIC_CONFIG_IMAGE_URL || "default.hostname.com",
         port: "",
         pathname: "/media/**",
-        search: "",
       },
     ],
   },
 };
 
-const withNextIntl = createNextIntlPlugin();
 export default withNextIntl(nextConfig);
